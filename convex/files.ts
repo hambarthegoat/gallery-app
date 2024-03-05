dd84c9737d93596856b6359725f607040943d57a
hambarthegoat <hipotesauce@gmail.com>
2024-03-05 23:41:58 +0700

    Menambahkan fitur menghapus data

--- convex/files.ts
+++ convex/files.ts
@@ -76,3 +76,29 @@ export const getFile = query({
         .collect();
     },
 });
+
+export const deleteFile = mutation({
+    args: {fileId: v.id("files") },
+    async handler(ctx, args){
+
+        const identity = await ctx.auth.getUserIdentity();
+        
+        if(!identity){
+            throw new ConvexError("You do not have access to this org");
+        }
+
+        const file = await ctx.db.get(args.fileId);
+
+        if(!file){
+            throw new ConvexError("This file do not exist");
+        }
+
+        const hasAccess = await hasAccessToOrg(ctx, identity.tokenIdentifier, file.orgId);
+
+        if(!hasAccess){
+            throw new ConvexError("You do not have access to delete this file")
+        }
+        await ctx.db.delete(args.fileId);
+        
+    },
+})
